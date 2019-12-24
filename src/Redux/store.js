fab78c07a195c6b5459dcc7bf98498d6a3921533
Mansoor <hussainmansoor876@gmail.com>
2019-12-24 17:46:32 +0500

    Set Redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,20 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const persistConfig = {
+    key: 'root',
+    storage
+}
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// const store = createStore(rootReducer, applyMiddleware(thunk))
+
+const store = createStore(persistedReducer,applyMiddleware(thunk))
+
+const persistor = persistStore(store)
+
+export { store, persistor };
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import store from "./Redux/store";
+import { store, persistor } from "./Redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
