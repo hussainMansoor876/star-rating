0855e65dbeb8096345ad3b8018e578bea2d2f0de
Mansoor <hussainmansoor876@gmail.com>
2020-01-14 23:37:59 +0500

    footer1

--- src/Screens/Header/Footer.js
+++ src/Screens/Header/Footer.js
@@ -7,61 +7,61 @@ var $ = require("jquery");
 
 
 class Footer extends Component {
-  constructor(props) {
-    super(props)
-  }
+    constructor(props) {
+        super(props)
+    }
 
 
 
-  render() {
+    render() {
 
-    return (
-      <div>
-                  <footer>
-          <div className="wrapper">
-            <div className="sec-padding">
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="footer-list">
-                    <h5 className="fc-white">Your Logo Here</h5>
-                    <p className="fc-white ff-primary">Online shops are checked for compliance with the Trusted Shops quality criteria before they are awarded the trustmark that they can then display on their website. Our quality criteria
-                       involve strict requirements as to the service quality as well as legal requirements.
+        return (
+            <div>
+                <footer>
+                    <div className="wrapper">
+                        <div className="sec-padding">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="footer-list">
+                                        <h5 className="fc-white">Your Logo Here</h5>
+                                        <p className="fc-white ff-primary">Online shops are checked for compliance with the Trusted Shops quality criteria before they are awarded the trustmark that they can then display on their website. Our quality criteria
+                                           involve strict requirements as to the service quality as well as legal requirements.
                             </p>
-                    <ul>
-                      <li><Link to="/contact">Contact Us</Link></li>
-                      <li><Link to="/privacy">Privacy Policy</Link></li>
+                                        <ul>
+                                            <li><Link to="/contact">Contact Us</Link></li>
+                                            <li><Link to="/privacy">Privacy Policy</Link></li>
 
-                    </ul>
-                    <span className="footer-social">
-                      <a href="#"><i className="fa fa-facebook"></i></a>
-                      <a href="#"><i className="fa fa-twitter"></i></a>
-                      <a href="#"><i className="fa fa-instagram"></i></a>
-                      <a href="#"><i className="fa fa-linkedin"></i></a>
-                    </span>
-                  </div>
-                </div>
-              </div>
+                                        </ul>
+                                        <span className="footer-social">
+                                            <a href="#"><i className="fa fa-facebook"></i></a>
+                                            <a href="#"><i className="fa fa-twitter"></i></a>
+                                            <a href="#"><i className="fa fa-instagram"></i></a>
+                                            <a href="#"><i className="fa fa-linkedin"></i></a>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
-          </div>
-        </footer>
-      </div>
 
-    )
-  }
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
 
-  console.log("mapToState", state.authReducer)
-  return {
-    user: state.authReducer.user,
-  }
+    console.log("mapToState", state.authReducer)
+    return {
+        user: state.authReducer.user,
+    }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    removeUser: () => dispatch(removeUser()),
-  }
+    return {
+        removeUser: () => dispatch(removeUser()),
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Footer)
