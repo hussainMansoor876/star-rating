d4bc049a21238e4c28f2d27ee858ac2fddf913a1
Mansoor <hussainmansoor876@gmail.com>
2020-01-16 20:19:36 +0500

    ADded country

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import Routes from './Config/routes'
+import data from './country'
+
+console.log('data', data)
 
 class App extends React.Component {
   render(){
