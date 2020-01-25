99c1c5224cc0e722cf328632aff6433d7cebf678
Mansoor <hussainmansoor876@gmail.com>
2020-01-26 00:52:01 +0500

    done

--- src/Screens/Search/Search.js
+++ src/Screens/Search/Search.js
@@ -18,7 +18,7 @@ class Search extends React.Component {
     super(props)
     this.state = {
       data: [],
-      result: true
+      result: true,
     }
   }
 
@@ -46,6 +46,7 @@ class Search extends React.Component {
 
   render() {
     const { data, result } = this.state
+    const { state } = this.props.history.location
     return (
       <div class="search-page">
         <Header {...this.props} />
@@ -66,64 +67,123 @@ class Search extends React.Component {
             {data.length && result ?
               <div className="sec-padding">
                 <center><h1>Search Result!!!</h1></center>
-                {data.map((v, i) => {
-                  return (
-                    <div className="search-box" key={i}>
-                      <div className="row">
-                        <div className="col-lg-4 col-md-4 col-sm-4 search-b-right">
-                          <div className="search-img">
-                            <img src={v.profilePic.url} />
-                            <h5 className="ff-secondary">{v.name}</h5>
-                            <h6 className="ff-secondary">Reviews: 50,000,00 <i className="fa fa-star"></i></h6>
-                          </div>
-                        </div>
-                        <div className="col-lg-8 col-md-8 col-sm-8">
-                          <div className="search-content">
-                            <div className="company-intro">
-                              <h5 className="ff-primary"><Link to={v.url}>{v.url}</Link></h5>
-                              <span className="search-stars-icon">
-                                <i className="fa fa-star"></i>
-                                <i className="fa fa-star"></i>
-                                <i className="fa fa-star"></i>
-                                <i className="fa fa-star"></i>
-                                <i className="fa fa-star"></i>
-                              </span>
-                              <p className="ff-primary">
-                                {v.description}
-                              </p>
+                {state.searchType === 'company' ?
+                  data.map((v, i) => {
+                    return (
+                      <div className="search-box" key={i}>
+                        <div className="row">
+                          <div className="col-lg-4 col-md-4 col-sm-4 search-b-right">
+                            <div className="search-img">
+                              <img src={v.profilePic.url} />
+                              <h5 className="ff-secondary">{v.name}</h5>
+                              <h6 className="ff-secondary">Reviews: 50,000,00 <i className="fa fa-star"></i></h6>
                             </div>
-                            <div className="row">
-                              <div className="col-lg-6 col-md-5">
-                                <div className="search-company-contact">
-                                  <h5 className="ff-secondary">Contact Information :</h5>
-                                  <p className="ff-primary">{v.address}</p>
-                                  <p className="ff-primary">{v.contactEmail}</p>
-                                  <p className="ff-primary"> {v.contactNo}</p>
-                                  <p className="search-social-icon">
-                                    <a href="#"> <i className="fa fa-facebook"></i></a>
-                                    <a href="#"> <i className="fa fa-twitter"></i></a>
-                                    <a href="#"> <i className="fa fa-instagram"></i></a>
-                                    <a href="#"> <i className="fa fa-linkedin"></i></a>
-                                  </p>
-
-                                </div>
+                          </div>
+                          <div className="col-lg-8 col-md-8 col-sm-8">
+                            <div className="search-content">
+                              <div className="company-intro">
+                                <h5 className="ff-primary"><Link to={v.url}>{v.url}</Link></h5>
+                                <span className="search-stars-icon">
+                                  <i className="fa fa-star"></i>
+                                  <i className="fa fa-star"></i>
+                                  <i className="fa fa-star"></i>
+                                  <i className="fa fa-star"></i>
+                                  <i className="fa fa-star"></i>
+                                </span>
+                                <p className="ff-primary">
+                                  {v.description}
+                                </p>
                               </div>
-                              <div className="col-lg-6 col-md-7">
-                                <div className="search-company-time">
-                                  <h5 className="ff-secondary">Location:</h5>
-                                  <p className="ff-primary">Country: {v.country}</p>
-                                  <p className="ff-primary">City: {v.city}</p>
-                                  <a href="#" className="ff-primary"><button> Read More </button></a>
-                                </div>
-                              </div>
+                              <div className="row">
+                                <div className="col-lg-6 col-md-5">
+                                  <div className="search-company-contact">
+                                    <h5 className="ff-secondary">Contact Information :</h5>
+                                    <p className="ff-primary">{v.address}</p>
+                                    <p className="ff-primary">{v.contactEmail}</p>
+                                    <p className="ff-primary"> {v.contactNo}</p>
+                                    <p className="search-social-icon">
+                                      <a href="#"> <i className="fa fa-facebook"></i></a>
+                                      <a href="#"> <i className="fa fa-twitter"></i></a>
+                                      <a href="#"> <i className="fa fa-instagram"></i></a>
+                                      <a href="#"> <i className="fa fa-linkedin"></i></a>
+                                    </p>
 
+                                  </div>
+                                </div>
+                                <div className="col-lg-6 col-md-7">
+                                  <div className="search-company-time">
+                                    <h5 className="ff-secondary">Location:</h5>
+                                    <p className="ff-primary">Country: {v.country}</p>
+                                    <p className="ff-primary">City: {v.city}</p>
+                                    <a href="#" className="ff-primary"><button> Read More </button></a>
+                                  </div>
+                                </div>
+
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  )
-                })}
+                    )
+                  }) :
+                  data.map((v, i) => {
+                    return (
+                      <div className="search-box" key={i}>
+                        <div className="row">
+                          <div className="col-lg-4 col-md-4 col-sm-4 search-b-right">
+                            <div className="search-img">
+                              <img src={v.profilePic.url} />
+                              <h5 className="ff-secondary">{v.name}</h5>
+                              <h6 className="ff-secondary">Reviews: 50,000,00 <i className="fa fa-star"></i></h6>
+                            </div>
+                          </div>
+                          <div className="col-lg-8 col-md-8 col-sm-8">
+                            <div className="search-content">
+                              <div className="company-intro">
+                                <h5 className="ff-primary"><Link to={v.url}>{v.url}</Link></h5>
+                                <span className="search-stars-icon">
+                                  <i className="fa fa-star"></i>
+                                  <i className="fa fa-star"></i>
+                                  <i className="fa fa-star"></i>
+                                  <i className="fa fa-star"></i>
+                                  <i className="fa fa-star"></i>
+                                </span>
+                                <p className="ff-primary">
+                                  {v.description}
+                                </p>
+                              </div>
+                              <div className="row">
+                                <div className="col-lg-6 col-md-5">
+                                  <div className="search-company-contact">
+                                    <h5 className="ff-secondary">Contact Information :</h5>
+                                    <p className="ff-primary">{v.address}</p>
+                                    <p className="ff-primary">{v.email}</p>
+                                    <p className="ff-primary"> {v.contactNo}</p>
+                                    <p className="search-social-icon">
+                                      <a href="#"> <i className="fa fa-facebook"></i></a>
+                                      <a href="#"> <i className="fa fa-twitter"></i></a>
+                                      <a href="#"> <i className="fa fa-instagram"></i></a>
+                                      <a href="#"> <i className="fa fa-linkedin"></i></a>
+                                    </p>
+
+                                  </div>
+                                </div>
+                                <div className="col-lg-6 col-md-7">
+                                  <div className="search-company-time">
+                                    <h5 className="ff-secondary">Location:</h5>
+                                    <p className="ff-primary">Country: {v.country}</p>
+                                    <p className="ff-primary">City: {v.city}</p>
+                                    <a href="#" className="ff-primary"><button> Read More </button></a>
+                                  </div>
+                                </div>
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  })}
               </div> :
               !data.length && !result ?
                 <center><h1 style={{ marginTop: 120, marginBottom: 120 }}>OOPS No result found</h1></center>
