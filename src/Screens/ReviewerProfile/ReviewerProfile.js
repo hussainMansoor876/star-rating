f03459a7cd11c9a2b5b619af3603dbccfe22fb92
Mansoor <hussainmansoor876@gmail.com>
2020-01-28 19:24:34 +0500

    working on review profile

--- src/Screens/ReviewerProfile/ReviewerProfile.js
+++ src/Screens/ReviewerProfile/ReviewerProfile.js
@@ -7,6 +7,7 @@ import manicon from '../../assets/img/man-icon-2.png';
 import logonew from '../../assets/img/new-logo.png';
 import Header from '../Header/Header'
 import Footer from '../Header/Footer'
+import { Skeleton } from 'antd';
 import axios from 'axios'
 
 
@@ -41,9 +42,8 @@ class Reviewer extends React.Component {
 		const { user } = this.state
 		if (!user) {
 			return (
-				<div className="main-body">
+				<div>
 					<Header {...this.props} />
-					<h1>Hello</h1>
 				</div>
 			)
 		}
@@ -79,7 +79,7 @@ class Reviewer extends React.Component {
 										<div className="row">
 											<div className="col-lg-12">
 												<div className="proven-con">
-													<img src={businesss} />
+													<img src={user.profilePic.url} />
 												</div>
 											</div>
 											<div className="col-lg-12">
@@ -95,15 +95,6 @@ class Reviewer extends React.Component {
 														</ul>
 													</div>
 													<h4 className="ff-secondary">{user.name}</h4>
-													<h6 className="ff-secondary">3.853 Bewertungen</h6>
-													<div className="pproven-bottom-last">
-														<span className="line-top"></span>
-														<span className="lie-botm"></span>
-														<a className="a-tg-h ff-primary" href="#"><span>637</span>
-															Reviews <br></br> </a>
-														<a className="a-tg-h ff-primary" href="#"><span>4</span>
-															other sources</a>
-													</div>
 												</div>
 											</div>
 										</div>
@@ -112,8 +103,8 @@ class Reviewer extends React.Component {
 								<div className="col-lg-10">
 									<div className="exp-main">
 										<div className="exp-con">
-											<h3 className="ff-secondary">ProvenExpert.com</h3>
-											<h4 className="ff-secondary">Customer satisfaction analysis & referral marketing</h4>
+											<h3 className="ff-secondary">{user.name.toUpperCase()}</h3>
+
 										</div>
 
 
@@ -122,41 +113,23 @@ class Reviewer extends React.Component {
 												<div className="col-lg-7 flrt-r">
 													<div className="center-first">
 														<div className="col-lg-12">
-															<div className="center-first-sub ">
-																<p className="ff-primary ideal">The ideal solution for your referral marketing: For more confidence, customers & sales.</p>
-																<p className="ff-primary">With ProvenExpert.com you get the feedback from your customers
-											 and present your evaluations from all platforms in one single rating...</p>
-																<a href="#">View full description <i className="fa fa-chevron-circle-down fc-blue"></i></a>
-															</div>
 														</div>
 														<div className="col-lg-6 col-md-6 flrt-r">
 															<div className="centr-sub-1">
 																<div className="centr-sub-1-main">
 																	<h5 className="ff-secondary">Contact information</h5>
-																	<span className="ff-primary">ProvenExpert.com</span>
-																	<span className="ff-primary">Quedlinburger Straße 1</span>
-																	<span className="ff-primary">10589 Berlin</span>
-																	<span className="ff-primary">Germany</span>
-																	<span className="ff-primary direction"><i className="fa fa-map-marker"></i><a href="#" className="fc-blue"> Direction</a></span>
+																	<span className="ff-primary">{user.email}</span>
+																	<h6 className="ff-secondary">3.853 Bewertungen</h6>
+																	<div className="pproven-bottom-last">
+																		<span className="line-top"></span>
+																		<span className="lie-botm"></span>
+																		<a className="a-tg-h ff-primary" href="#"><span>637</span>
+																			Reviews <br></br> </a>
+																		<a className="a-tg-h ff-primary" href="#"><span>4</span>
+																			other sources</a>
+																	</div>
 																</div>
 
-															</div>
-														</div>
-														<div className="col-lg-6 col-md-6 flrt-r">
-															{/* <div className="centr-sub-2">
-											<h5 className="ff-secondary">Opening hours</h5>
-											<span className="col-lg-6 col-xs-6 ff-primary">Donnerstag</span>
-											<span className="col-lg-6 col-xs-6 ff-primary">09:00-18:00</span>
-											<a className="col-lg-12 ff-primary" href="#">Alle Zeiten anzeigen</a>
-										</div> */}
-															<div className="centr-sub-2-main main-2">
-																<h5 className="ff-secondary">Contact person</h5>
-																<span className="ff-primary">Remo Fyda</span>
-																<ul>
-																	<li className="ff-primary"><i className="fa fa-phone"></i><a href="#" className="fc-blue">030 270 041 905</a></li>
-																	<li className="ff-primary"><i className="fa fa-phone"></i><a href="#"> 030 270 041 400</a></li>
-																	<li className="ff-primary"><i className="fa fa-envelope"></i><a href="#" className="fc-blue"> E-Mail sender</a></li>
-																</ul>
 															</div>
 														</div>
 													</div>
@@ -165,196 +138,12 @@ class Reviewer extends React.Component {
 													<div className="center-second">
 														<div className="center-second-main">
 															{/* <p className="ff-primary">Bewertungen vom 14.12.2019 </p> */}
-															<div className="row">
-																<div className="col-lg-6 col-md-6">
-																	<div className="for-main">
-																		<span className="ff-primary">5,00 von 5</span>
-																		<div className="starr">
-																			<ul>
-																				<li><i className="fa fa-star"></i></li>
-																				<li><i className="fa fa-star"></i></li>
-																				<li><i className="fa fa-star"></i></li>
-																				<li><i className="fa fa-star"></i></li>
-																				<li><i className="fa fa-star"></i></li>
-																			</ul>
-																		</div>
-																		<h4 className="ff-secondary">SEHR GUT</h4>
-																	</div>
-																</div>
-																<div className="col-lg-6 col-md-6">
-																	<div className="icon-image">
-																		<img src={manicon} />
-																		<h5>recommendation</h5>
-																	</div>
-
-																</div>
-															</div>
-															<div className="star-rating">
-																<div className="star-first">
-																	<div className="row">
-																		<div className="col-lg-8 col-md-8 col-sm-8 col-8">
-																			<p className="ff-primary fc-blue"><a href="#">Possible applications</a></p>
-																		</div>
-																		<div className="col-lg-4 col-md-4 col-sm-4 col-4">
-																			<div className="starrs">
-																				<ul>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																				</ul>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<div className="star-first">
-																	<div className="row">
-																		<div className="col-lg-8 col-md-8 col-sm-8 col-8">
-																			<p className="ff-primary fc-blue"><a href="#">Features</a></p>
-																		</div>
-																		<div className="col-lg-4 col-md-4 col-sm-4 col-4">
-																			<div className="starrs">
-																				<ul>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																				</ul>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<div className="star-first">
-																	<div className="row">
-																		<div className="col-lg-8 col-md-8 col-sm-8 col-8">
-																			<p className="ff-primary fc-blue"><a href="#">Transparency</a></p>
-																		</div>
-																		<div className="col-lg-4 col-md-4 col-sm-4 col-4">
-																			<div className="starrs">
-																				<ul>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																				</ul>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<div className="star-first">
-																	<div className="row">
-																		<div className="col-lg-8 col-md-8 col-sm-8 col-8">
-																			<p className="ff-primary fc-blue"><a href="#">Privacy</a></p>
-																		</div>
-																		<div className="col-lg-4 col-md-4 col-sm-4 col-4">
-																			<div className="starrs">
-																				<ul>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																				</ul>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<div className="star-first">
-																	<div className="row">
-																		<div className="col-lg-8 col-md-8 col-sm-8 col-8">
-																			<p className="ff-primary fc-blue"><a href="#">Value</a></p>
-																		</div>
-																		<div className="col-lg-4 col-md-4 col-sm-4 col-4">
-																			<div className="starrs">
-																				<ul>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																					<li><i className="fa fa-star"></i></li>
-																				</ul>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-															</div>
 														</div>
 													</div>
 												</div>
 											</div>
 										</div>
 
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-
-
-				<section id="competencies">
-					<div className="wrapper">
-						<div className="competencies-main">
-							<p className="ff-primary"><span >TOP COMPETENCIES</span> (Based on 41 competencies)</p>
-							<a href="#"><i className="fa fa-chevron-circle-down fc-blue"></i></a>
-						</div>
-					</div>
-				</section>
-
-
-				<section id="measure">
-					<div className="wrapper">
-						<div className="measure-main">
-							<div className="row">
-								<div className="col-lg-4">
-									<div className="measure-sub">
-										<h5><i className="fa fa-check"></i>Measure of satisfaction</h5>
-										<span className="ff-primary">4.66/5.00</span>
-										<div className="starres">
-											<ul>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-											</ul>
-										</div>
-										<span>701 Reviews</span>
-									</div>
-								</div>
-								<div className="col-lg-4">
-									<div className="measure-sub">
-										<h5><i className="fa fa-check"></i>Measure of satisfaction</h5>
-										<span className="ff-primary">4.66/5.00</span>
-										<div className="starres">
-											<ul>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-											</ul>
-										</div>
-										<span className="ff-primary">701 Reviews</span>
-									</div>
-								</div>
-								<div className="col-lg-4">
-									<div className="measure-sub">
-										<h5><i className="fa fa-check"></i>Measure of satisfaction</h5>
-										<span className="ff-primary">4.66/5.00</span>
-										<div className="starres">
-											<ul>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-												<li><i className="fa fa-star"></i></li>
-											</ul>
-										</div>
-										<span className="ff-primary">701 Reviews</span>
 									</div>
 								</div>
 							</div>
@@ -368,7 +157,7 @@ class Reviewer extends React.Component {
 							<div className="row">
 								<div className="col-lg-8 col-md-8 col-sm-12">
 									<div className="reviews-head">
-										<h5 className="fc-blue">3,218 Reviews on ProvenExpert.com</h5>
+										<h5 className="fc-blue">3,218 Reviews on Star Rating</h5>
 									</div>
 								</div>
 								<div className="col-lg-2 col-md-4 col-sm-12">
