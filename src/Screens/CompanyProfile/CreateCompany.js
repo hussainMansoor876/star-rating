c22dee29e3e742c31e6edd0cd61c0efd941843ba
Mansoor <hussainmansoor876@gmail.com>
2020-02-01 19:24:52 +0500

    company done

--- src/Screens/CompanyProfile/CreateCompany.js
+++ src/Screens/CompanyProfile/CreateCompany.js
@@ -10,6 +10,7 @@ import { Link } from 'react-router-dom'
 import Footer from '../Header/Footer'
 import data from '../../country'
 import Plan from '../Plan/Plan'
+import Exception from 'ant-design-pro/lib/Exception';
 
 const { Option } = Select;
 
@@ -165,237 +166,239 @@ class CreateCompany extends React.Component {
 
 	render() {
 		const { getFieldDecorator } = this.props.form;
-		const { city } = this.state
-
+		const { city, success } = this.state
+		const { user } = this.props
 		return (
 			<div className="main-body">
 				<Header {...this.props} />
+				{!success && user.buyPlan ? <Exception type="500" title="Company" style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: '100px' }} desc="Your Company profile is on pending!!!" /> :
+					<div>
+						<section id="banner-6">
+							<div className="wrapper">
+								<div className="d-table w-100">
+									<div className="d-table-cell va-middle">
+										<div className="banner-content">
+											<h2 className="ff-secondary">Register Your Company</h2>
+											<p className="ff-primary fc-white">The free version of ProvenExpert is — and always will be — free.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
 
-				<section id="banner-6">
-					<div className="wrapper">
-						<div className="d-table w-100">
-							<div className="d-table-cell va-middle">
-								<div className="banner-content">
-									<h2 className="ff-secondary">Register Your Company</h2>
-									<p className="ff-primary fc-white">The free version of ProvenExpert is — and always will be — free.</p>
+						<div className="create-card">
+							<div className="wrapper">
+								<div className="card">
+									<div>
+										<Form onSubmit={this.handleSubmit} className="login-form">
+											<h1 className="ff-secondary fc-blue" style={{ textAlign: 'center' }} >Company Registration Form</h1>
+											<div className="row">
+												<div className="col-md-6 col-sm-6 col-xs-12">
+													<label>Company Name:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('name', {
+															rules: [{ required: true, message: 'Please input your Company Name!' }],
+														})(
+															<Input
+																name="name"
+																prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+																placeholder="Company Name"
+															/>,
+														)}
+													</Form.Item>
+												</div>
+												<div className="col-md-6 col-sm-6 col-xs-12">
+													<label>Company Url:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('url', {
+															rules: [{ required: true, message: 'Please input your Company Url!' }],
+														})(
+															<Input
+																name="name"
+																prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+																placeholder="example.com"
+															/>,
+														)}
+													</Form.Item>
+												</div>
+												<div className="col-md-6 col-sm-6 col-xs-12">
+													<label>Telephone Number:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('telnumber', {
+															rules: [{ required: true, message: 'Please input your Tel Number!' }],
+														})(
+
+															<Input
+																type="phone"
+																name="phone"
+																prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+																placeholder="Telephone No"
+															/>,
+														)}
+													</Form.Item>
+												</div>
+												<div className="col-md-6 col-sm-6 col-xs-12">
+													<label>Mobile Number:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('contactNo', {
+															rules: [{ required: true, message: 'Please input your Mob Number!' }],
+														})(
+															<Input
+																type="phone"
+																name="phone"
+																prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+																placeholder="Mobile No"
+															/>,
+														)}
+													</Form.Item>
+												</div>
+												<div className="col-md-6 col-sm-6 col-xs-12">
+													<label>Email:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('contactEmail', {
+															rules: [{ required: true, message: 'Please input your Email!' }],
+														})(
+															<Input
+																disabled
+																name="email"
+																prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+																type="email"
+																placeholder="Email"
+															/>,
+														)}
+													</Form.Item>
+												</div>
+
+												<div className="col-md-6 col-sm-6 col-xs-12">
+													<label>Address:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('address', {
+															rules: [{ required: true, message: 'Please input your Address!' }],
+														})(
+															<Input
+																type="name"
+																name="name"
+																prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+																placeholder="Address"
+															/>,
+														)}
+													</Form.Item>
+
+												</div>
+												<div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+													<label>Country:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('country', {
+															rules: [{ required: true, message: 'Please Select Your Country!' }],
+														})(
+															<Select
+																showSearch
+																// style={{ width: 200 }}
+																style={{ backgroundColor: '#fff' }}
+																placeholder="Select a Country"
+																optionFilterProp="children"
+																onSelect={(e) => this.setState({ city: data[e] })}
+																filterOption={(input, option) =>
+																	option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+																}
+															>
+																{
+																	Object.keys(data).map((v, i) => {
+																		return <Option value={v} key={i}>{v}</Option>
+																	})
+																}
+															</Select>,
+														)}
+													</Form.Item>
+
+												</div>
+												<div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+													<label>City:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('city', {
+															rules: [{ required: true, message: 'Please Select Your City!' }],
+														})(
+															<Select
+																showSearch
+																style={{ backgroundColor: '#fff' }}
+																placeholder="Select a city"
+																optionFilterProp="children"
+																onSelect={(e) => console.log(e)}
+																filterOption={(input, option) =>
+																	option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+																}
+															>
+																{
+																	city.map((v, i) => {
+																		return <Option city={v} key={i}>{v}</Option>
+																	})
+																}
+															</Select>,
+														)}
+													</Form.Item>
+
+												</div>
+												<div className="col-md-6 col-sm-6 col-xs-12">
+													<label>Company Title:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('title', {
+															rules: [{ required: true, message: 'Please input your Company Title!' }],
+														})(
+															<Input
+																name="title"
+																prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+																placeholder="Company Title"
+															/>,
+														)}
+													</Form.Item>
+												</div>
+												<div className="col-md-6 col-sm-6 col-xs-12">
+													<label>Upload Your Picture</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('profilePic', {
+															valuePropName: 'fileList',
+															getValueFromEvent: this.normFile,
+														})(
+															<Upload name="logo" accept="image/*" disabled={this.state.disable}>
+																<Button disabled={this.state.disableUpload}>
+																	<Icon type="upload" /> Click to upload Image
+                    									</Button>
+															</Upload>,
+														)}
+													</Form.Item>
+												</div>
+												<div className="col-md-12">
+													<label>Description:</label>
+													<Form.Item className="sign-up">
+														{getFieldDecorator('description', {
+															rules: [{ required: true, message: 'Please input your Description!' }],
+														})(
+															<TextArea
+																placeholder="Description"
+															/>
+														)}
+													</Form.Item>
+												</div>
+
+												<div className="col-md-12">
+													<div className="register-btn">
+														<Form.Item className="sign-up">
+															<Button htmlType="submit" className="login-form-button" style={{ backgroundColor: '#37A000', color: 'white', fontWeight: 'bold', fontSize: 14, height: 40 }}>
+																Register
+												</Button>
+														</Form.Item>
+													</div>
+												</div>
+											</div>
+
+
+										</Form>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</section>
-
-				<div className="create-card">
-					<div className="wrapper">
-						<div className="card">
-							<div>
-								<Form onSubmit={this.handleSubmit} className="login-form">
-									<h1 className="ff-secondary fc-blue" style={{ textAlign: 'center' }} >Company Registration Form</h1>
-									<div className="row">
-										<div className="col-md-6 col-sm-6 col-xs-12">
-											<label>Company Name:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('name', {
-													rules: [{ required: true, message: 'Please input your Company Name!' }],
-												})(
-													<Input
-														name="name"
-														prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-														placeholder="Company Name"
-													/>,
-												)}
-											</Form.Item>
-										</div>
-										<div className="col-md-6 col-sm-6 col-xs-12">
-											<label>Company Url:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('url', {
-													rules: [{ required: true, message: 'Please input your Company Url!' }],
-												})(
-													<Input
-														name="name"
-														prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-														placeholder="example.com"
-													/>,
-												)}
-											</Form.Item>
-										</div>
-										<div className="col-md-6 col-sm-6 col-xs-12">
-											<label>Telephone Number:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('telnumber', {
-													rules: [{ required: true, message: 'Please input your Tel Number!' }],
-												})(
-
-													<Input
-														type="phone"
-														name="phone"
-														prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-														placeholder="Telephone No"
-													/>,
-												)}
-											</Form.Item>
-										</div>
-										<div className="col-md-6 col-sm-6 col-xs-12">
-											<label>Mobile Number:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('contactNo', {
-													rules: [{ required: true, message: 'Please input your Mob Number!' }],
-												})(
-													<Input
-														type="phone"
-														name="phone"
-														prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-														placeholder="Mobile No"
-													/>,
-												)}
-											</Form.Item>
-										</div>
-										<div className="col-md-6 col-sm-6 col-xs-12">
-											<label>Email:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('contactEmail', {
-													rules: [{ required: true, message: 'Please input your Email!' }],
-												})(
-													<Input
-														disabled
-														name="email"
-														prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-														type="email"
-														placeholder="Email"
-													/>,
-												)}
-											</Form.Item>
-										</div>
-
-										<div className="col-md-6 col-sm-6 col-xs-12">
-											<label>Address:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('address', {
-													rules: [{ required: true, message: 'Please input your Address!' }],
-												})(
-													<Input
-														type="name"
-														name="name"
-														prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-														placeholder="Address"
-													/>,
-												)}
-											</Form.Item>
-
-										</div>
-										<div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-											<label>Country:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('country', {
-													rules: [{ required: true, message: 'Please Select Your Country!' }],
-												})(
-													<Select
-														showSearch
-														// style={{ width: 200 }}
-														style={{ backgroundColor: '#fff' }}
-														placeholder="Select a Country"
-														optionFilterProp="children"
-														onSelect={(e) => this.setState({ city: data[e] })}
-														filterOption={(input, option) =>
-															option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-														}
-													>
-														{
-															Object.keys(data).map((v, i) => {
-																return <Option value={v} key={i}>{v}</Option>
-															})
-														}
-													</Select>,
-												)}
-											</Form.Item>
-
-										</div>
-										<div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-											<label>City:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('city', {
-													rules: [{ required: true, message: 'Please Select Your City!' }],
-												})(
-													<Select
-														showSearch
-														style={{ backgroundColor: '#fff' }}
-														placeholder="Select a city"
-														optionFilterProp="children"
-														onSelect={(e) => console.log(e)}
-														filterOption={(input, option) =>
-															option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-														}
-													>
-														{
-															city.map((v, i) => {
-																return <Option city={v} key={i}>{v}</Option>
-															})
-														}
-													</Select>,
-												)}
-											</Form.Item>
-
-										</div>
-										<div className="col-md-6 col-sm-6 col-xs-12">
-											<label>Company Title:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('title', {
-													rules: [{ required: true, message: 'Please input your Company Title!' }],
-												})(
-													<Input
-														name="title"
-														prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-														placeholder="Company Title"
-													/>,
-												)}
-											</Form.Item>
-										</div>
-										<div className="col-md-6 col-sm-6 col-xs-12">
-											<label>Upload Your Picture</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('profilePic', {
-													valuePropName: 'fileList',
-													getValueFromEvent: this.normFile,
-												})(
-													<Upload name="logo" accept="image/*" disabled={this.state.disable}>
-														<Button disabled={this.state.disableUpload}>
-															<Icon type="upload" /> Click to upload Image
-                    									</Button>
-													</Upload>,
-												)}
-											</Form.Item>
-										</div>
-										<div className="col-md-12">
-											<label>Description:</label>
-											<Form.Item className="sign-up">
-												{getFieldDecorator('description', {
-													rules: [{ required: true, message: 'Please input your Description!' }],
-												})(
-													<TextArea
-														placeholder="Description"
-													/>
-												)}
-											</Form.Item>
-										</div>
-
-										<div className="col-md-12">
-											<div className="register-btn">
-												<Form.Item className="sign-up">
-													<Button htmlType="submit" className="login-form-button" style={{ backgroundColor: '#37A000', color: 'white', fontWeight: 'bold', fontSize: 14, height: 40 }}>
-														Register
-												</Button>
-												</Form.Item>
-											</div>
-										</div>
-									</div>
-
-
-								</Form>
-							</div>
-						</div>
-					</div>
-				</div>
-				<Footer {...this.props} />
+						<Footer {...this.props} />
+					</div>}
 			</div>
 		)
 	}
