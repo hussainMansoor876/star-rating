16a30c98fbca8a4f663787403df2da33012c28c0
Mansoor <hussainmansoor876@gmail.com>
2020-02-04 22:10:14 +0500

    done

--- src/Screens/Review/Review.js
+++ src/Screens/Review/Review.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Icon, Form, Modal, Upload, Input, Button, message } from 'antd';
+import { Icon, Form, Modal, Upload, Input, Button, message, notification } from 'antd';
 
 const { TextArea } = Input;
 
@@ -19,7 +19,7 @@ const props = {
             message.error(`${info.file.name} file upload failed.`);
         }
     },
-  }
+}
 
 class Review extends React.Component {
     constructor(props) {
@@ -29,11 +29,18 @@ class Review extends React.Component {
         }
     }
 
+    openNotification = (title, desc, icon, color = '#108ee9') => {
+        notification.open({
+          message: title,
+          description: desc,
+          icon: <Icon type={icon} style={{ color: color }} />,
+        });
+      };
+
     normFile = e => {
-        const { openNotification } = this.props
         this.setState({ disableUpload: false })
-        if (e.file.type.indexOf('image')) {
-            openNotification('Error', 'Please Upload an Image or video!', 'close-circle', 'red')
+        if (e.file.type.indexOf('video')) {
+            this.openNotification('Error', 'Please Upload a Video', 'close-circle', 'red')
             return
         }
         if (Array.isArray(e)) {
@@ -58,18 +65,18 @@ class Review extends React.Component {
                 onOk={onCreate}
             >
                 <Form layout="vertical">
-                    <Form.Item label="Description">
-                        {getFieldDecorator('Review', {
+                    <Form.Item label="Review">
+                        {getFieldDecorator('review', {
                             rules: [{ required: true, message: 'Please Add the description of Image!' }]
                         })(<TextArea rows={3} />)}
                     </Form.Item>
                     <Form.Item >
-                        {getFieldDecorator('file', {
+                        {getFieldDecorator('video', {
                             rules: [{ required: false, message: 'Please Upload the Image!' }],
                             valuePropName: 'fileList',
                             getValueFromEvent: this.normFile,
                         })(
-                            <Upload name="logo" listType="picture" accept="image/*" {...props}>
+                            <Upload name="logo" listType="picture" accept="video/*" {...props}>
                                 <Button disabled={disableUpload}>
                                     <Icon type="upload" /> Click to upload
                       </Button>
